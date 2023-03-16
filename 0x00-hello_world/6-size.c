b3968201dc4bc9427b06ff1309e767636a49f83b
momo8855 <hanywawe@gmail.com>
2023-03-16 13:57:17 -0700

    djks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: sizeof
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("The size of char is %zu\n", sizeof(char));
+	return (0);
+}
