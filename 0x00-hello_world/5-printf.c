f79876f951c145a17100febc5981a8db96590c7f
momo8855 <hanywawe@gmail.com>
2023-03-16 13:47:15 -0700

    djls

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: use printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but outcome is a piece of art");
+	return (0);
+}
