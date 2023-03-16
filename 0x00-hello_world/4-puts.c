2382b173ff269629c8e5fa36308f8fe9c7d00a9c
momo8855 <hanywawe@gmail.com>
2023-03-16 13:38:11 -0700

    djsl

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: C program that prints a
+ *
+ * Return: Always  0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
