c66064030e7a9c8fb8598eb4f22f3ac20d066bff
momo8855 <hanywawe@gmail.com>
2023-03-18 14:00:44 -0700

    djsk

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - Entry Point
+  *
+  * Dscription: 2 loops
+  *
+  * Return: Alwyas 0 (Success)
+  */
+int main(void)
+{
+	char l, u;
+
+	for (l = 'a'; l <= 'z'; ++l)
+		putchar(l);
+	for (u = 'A'; u <= 'Z'; ++u)
+		putchar(u);
+
+	putchar('\n');
+}
