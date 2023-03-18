95b666f0334db04a4654554c4278cc7b2984d949
momo8855 <hanywawe@gmail.com>
2023-03-18 14:11:52 -0700

    Jkd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Description: print 0-9
+  *
+  * Return: 0 Always (Success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; ++n)
+		printf("%d", n);
+		printf("\n");
+
+	return (0);
+}
