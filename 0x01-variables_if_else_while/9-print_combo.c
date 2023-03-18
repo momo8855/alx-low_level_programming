bfeead9eb9aef9c98b7b2616954478e91a910735
momo8855 <hanywawe@gmail.com>
2023-03-18 14:23:04 -0700

    djk

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Description: print numbers sperated by comma and space
+  *
+  * Return: 0 always (Success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+		if (n != 9)
+			putchar(',');
+			putchar (' ');
+	}
+	putchar('\n');
+	return (0);
+}
