c77cdcac587b35361e79e0fe051507eef3f7d829
momo8855 <hanywawe@gmail.com>
2023-03-23 07:37:04 -0700

    jdks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: print the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	
+	while (i < 10)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
