6e7b2bbd5efd759c67efaa99ead14f304212d872
momo8855 <hanywawe@gmail.com>
2023-03-23 07:27:04 -0700

    dk

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_alphabet - use _putchar to print
+  *		the alphabet a - z
+ */
+void print_alphabet(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch)
+	}
+	_putchar('\n')
+}
