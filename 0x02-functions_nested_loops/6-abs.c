67a3009bda1a5b3dec505704cbf800cd4f91a397
momo8855 <hanywawe@gmail.com>
2023-03-23 18:33:23 -0700

    jdk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _abs - Entry Point
+  *
+  * Description: computes the absolute value
+  *
+  * @n: input to calclute the abs of
+  *
+  * Return: n
+  */
+int _abs(int n)
+{
+	abs(n);
+	return (n);
+}
