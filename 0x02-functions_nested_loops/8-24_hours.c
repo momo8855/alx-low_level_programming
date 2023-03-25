5bce0814d22b4a11a54c4349d09602e2a674e682
momo8855 <hanywawe@gmail.com>
2023-03-25 15:36:27 -0700

    kdj

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,8 +9,10 @@
   */
 void jack_bauer(void)
 {
-	for (int hour = 0; hour < 24; hour++)
-		for (int minute = 0; minute < 60; minute++)
+	int minute, hour;
+
+	for (hour = 0; hour < 24; hour++)
+		for (minute = 0; minute < 60; minute++)
 		{
 			_putchar("%02d:%02d\n", hour, minute);
 		}
