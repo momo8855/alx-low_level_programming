e584da816a534c7b1b1ff223190dc24b1c7d2a58
momo8855 <hanywawe@gmail.com>
2023-03-25 16:35:15 -0700

    dkj

---
+++ 0x02-functions_nested_loops/9-time_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * times_table - Entry Point
+  *
+  * Decription: wirte the 9 times table
+  *
+  * Return: 0 Always (Success)
+  */
+void times_table(void)
+{
+	int row, col;
+
+	for (row = 0; row < 10; row++)
+		for (col = 0; col < 10; col++)
+		{
+			product = row * col;
+			if (product < 10)
+			{
+				_putchar(product + 48);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(product + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+		_putchar('\n')
+		}
+}
