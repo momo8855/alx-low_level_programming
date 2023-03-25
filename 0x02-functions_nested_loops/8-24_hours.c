940046839e5fd051a4bd034c1f1d3a88fb6a4a56
momo8855 <hanywawe@gmail.com>
2023-03-25 15:56:47 -0700

    djk

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,6 +14,11 @@ void jack_bauer(void)
 	for (hour = 0; hour < 24; hour++)
 		for (minute = 0; minute < 60; minute++)
 		{
-			_putchar("%02d:%02d\n", hour, minute);
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((minute / 10) + 48);
+			_putchar((minute % 10) + 48);
+			_putchar('\n');
 		}
 }
