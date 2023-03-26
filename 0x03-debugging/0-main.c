4b98bf48c09b1836774a4454032b194b85645588
momo8855 <hanywawe@gmail.com>
2023-03-26 03:10:24 -0700

    KLDJ

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * main - Entry Point
+  *
+  * Description: check the zero input
+  *
+  * Return: 0 Always (Success)
+  */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
