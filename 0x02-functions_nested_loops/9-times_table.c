8e74cae4193fb7cea8e33569231c2e4de4a3a850
momo8855 <hanywawe@gmail.com>
2023-03-25 17:09:19 -0700

    dkjs

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,30 +13,25 @@ void times_table(void)
 
 	for (row = 0; row < 10; row++)
 	{
-		for (col = 0; col < 10; col++)
+		_putchar(48);
+		for (col = 1; col < 10; col++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			product = row * col;
+
 			if (product < 10)
 			{
-				_putchar(product + 48);
-				if (col != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar((product / 10) + 48);
 				_putchar((product % 10) + 48);
-				if (col != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
 		}
+	}
 		_putchar('\n');
 	}
 }
