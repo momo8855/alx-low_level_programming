bd4906772ffc79c64e3b2b676ffac43cff621e76
momo8855 <hanywawe@gmail.com>
2023-03-25 17:27:32 -0700

    Jkdl

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * add - Entry Point
+  *
+  * Description: add two numbers
+  *
+  * @n1: first input
+  *
+  * @n2: second input
+  *
+  * Return: result of addition
+  */
+int add(int n1, int n2)
+{
+	int result;
+
+	result = n1 + n2;
+
+	return (result);
+}
