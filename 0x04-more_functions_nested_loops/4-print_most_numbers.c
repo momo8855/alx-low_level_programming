1f3ae6dc317eea094b8d0159124255a0c2fee6a0
momo8855 <hanywawe@gmail.com>
2023-03-26 18:02:49 -0700

    djks

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - Entry Point
+  *
+  * Description: print from 0 to 9 except 2 and 4
+  *
+  * Return: 0 Always (Success)
+  */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + 48);
+		}
+	}
+	_putchar('\n');
+}
