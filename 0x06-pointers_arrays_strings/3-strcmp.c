b03d77ad36d50b06cb8dd719a0987cdb35d0d046
momo8855 <hanywawe@gmail.com>
2023-03-30 02:29:51 -0700

    kdjs

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,15 +12,21 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	int i = 0, r;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		*s1++;
-		*s2++;
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			r = s1[i] - s2[i];
+		}
+		i++;
 	}
-	if (*s1 == *s2)
-		return (0);
-	if (*s1 < *s2)
-		return (-1);
-	else
-		return (1);
+
+	return (r);
 }
