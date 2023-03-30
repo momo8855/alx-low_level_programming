067877ad23169bcaf25d14a06a696945c7507e96
momo8855 <hanywawe@gmail.com>
2023-03-30 01:52:01 -0700

    dks

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strcmp - comapres to strings
+  *
+  * @s1: first string
+  * @s2: second string
+  *
+  * Return: 0 if equal
+  *	    1 if first is bigger
+  *	    -1 if first is lower
+  */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		*s1++;
+		*s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	if (*s1 < *s2)
+		return (-1);
+	else
+		return (1);
+}
+
