cb050768ff8ea65567cb1a5a8117d926baeb5b4b
momo8855 <hanywawe@gmail.com>
2023-03-30 02:58:57 -0700

    jdks

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * *leet - encode
+  *
+  * @str: string to encode
+  *
+  * Return: encoded string
+  */
+char *leet(char *str)
+{
+	char *result = str;
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_replacements = "44330771";
+
+	for (int i = 0; leet_chars[i] != '\0'; i++)
+	{
+		for (int j = 0; result[j] != '\0'; j++)
+		{
+			if (result[j] == leet_chars[i])
+			{
+				result[j] = leet_replacements[i];
+			}
+		}
+	}
+	return (result);
+}
