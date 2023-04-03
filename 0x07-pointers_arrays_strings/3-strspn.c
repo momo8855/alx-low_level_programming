246c264d4f5f592ce38b28ae3fd9bed80e85caba
momo8855 <hanywawe@gmail.com>
2023-04-03 04:52:17 -0700

    dkjs

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _strspn - gets the length of a prefix substring
+  *
+  * @s: pointer to the string to search
+  * @accept: pointer to the set of characters to accept
+  *
+  * Return: the number of bytes in the initial segment of s
+  *		which consist only of bytes from accept
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	while (*s != '\0' && flag)
+	{
+		unsigned int count = 0;
+		int flag = 1;
+		char *a;
+
+		for (char *a == accept; *a != '\0'; a++)
+		{
+			if (*s == *a)
+			{
+				count++;
+				flag = 1;
+				break;
+			}
+		}
+		s++
+	}
+	return (count)
+
+}
