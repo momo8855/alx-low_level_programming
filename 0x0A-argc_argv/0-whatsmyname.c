8c7cc3d4d8b491c602211b3bc1e81103f87acfea
momo8855 <hanywawe@gmail.com>
2023-04-15 14:57:04 -0700

    ldskfj

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry Point
+  *
+  * @argc: number of arguments
+  * @argv: vector of arguments
+  *
+  * Return: Always 0 (Success)
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
