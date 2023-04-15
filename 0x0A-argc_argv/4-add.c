9fce1cc8c06f2bba1a3deb7a2cbd10276af5f1e9
momo8855 <hanywawe@gmail.com>
2023-04-15 15:59:22 -0700

    kdjls

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * isNum - check if string array is num
+ * @num: string to check
+ * Return: 0 if it's a number
+ *         1 if it's not a number
+*/
+int isNum(char num[])
+{
+	int i, l = strlen(num);
+
+	for (i = 0; i < l; i++)
+	{
+		if (!isdigit(num[i]))
+			return (1);
+	}
+	return (0);
+}
+
+/**
+  * main - Entry Point
+  *
+  * @argc: number of arguments
+  * @argv: vector of arguments
+  *
+  * Return: 0 or 1 
+  */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			if (isNum(argv[i]) == 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
