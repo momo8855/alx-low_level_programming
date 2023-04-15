0bbc1195133a46d19ee026196b5b5d5c2a590df2
momo8855 <hanywawe@gmail.com>
2023-04-15 07:58:50 -0700

    msldk

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+  * is_divisabel - check if divisable
+  *
+  * @n: number to be checked
+  * divisor: the divisor
+  * Return: returns 1 if n is divisible by divisor, otherwise it returns 0
+  */
+int is_divisable(int n, int divisor)
+{
+	if (divisor == 1)
+	{
+		return (0);
+	}
+	else if (n % divisor == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return is_divisable(n, divisor - 1);
+	}
+}
+
+/**
+  * int is_prime_helper - check if n is prime
+  *
+  * @n: number to be checked
+  * @divisor: the divisor to test
+  *
+  * Return: return 1 if n is prime and -1 otheriwse
+  */
+int is_prime_helper(int n, int divisor)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	else if (divisor == 1)
+	{
+		return (1);
+	}
+	else if (is_divisable(n, divisor - 1))
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime_helper(n, divisor - 1));
+	}
+}
+
+/**
+  * is_prime_numbers - check if n is prime
+  *
+  * @n: number to be checked
+  *
+  * Return: 1 if the input integer is a prime number, otherwise return 0
+  */
+int is_prime_number(int n)
+{
+	return (is_prime_helper(n, n-1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <math.h>
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
@@ -10,5 +11,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int sqrt_helper(int n, int low, int high);
 int _sqrt_recursion(int n);
-
+int is_divisable(int n, int divisor);
+int is_prime_helper(int n, int divisor);
+int is_prime_number(int n);
 #endif
