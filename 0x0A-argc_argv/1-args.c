a8c7966cf20c290ea3077d9315a946976f4b7ca4
momo8855 <hanywawe@gmail.com>
2023-04-15 15:07:23 -0700

    dklsj

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry Point
+  *
+  * @argc: the number of arguments
+  * @argv: the vector of aargumnts
+  *
+  * Return: 0 Always (success)
+  */
+int main(int argc,  char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
